0e9a928d5096f0f5de52c8acb3e4b8df8ec9f810
Vidyansh07 <Vidyansht@gmail.com>
2024-02-07 00:12:43 +0530

    Day-2 in java

---
+++ APPS/nested/main.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String args[]){
+        // title
+        // name 
+        // age 
+        // mobile
+        // password
+        // repeat password
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Please enter your Title Mr/Mrs/Miss/Shri:");
+        String title = scanner.nextLine();
+
+        System.out.println( "Please enter your Name : ");
+        String name = scanner.nextLine();
+
+        System.out.println("Please enter your Age : ");
+        int  age = scanner.nextInt();
+
+        System.out.println( "Please enter your Mobile Number :" );
+        String mob_num = scanner.nextLine();
+
+        System.out.println( "Please create a Password:" );
+        String pass1 = scanner.nextLine();
+
+        System.out.println( "Re-enter the Password to confirm:" );
+        String  pass2 = scanner.nextLine();
+
+        scanner.close();
+
+
+        // positive method
+
+        if (title.equals("Mr")
+        || title.equals("Mrs")
+        || title.equals("Miss")
+        || title.equals("Shri")){
+            if  (age >=18){
+                if (pass1.equals(pass2)){
+                    System.out.println("Signup Successfull");
+                }
+                else{
+                    System.out.println( "Passwords do not match. Please try again." );
+                }
+            
+                System.out.println( "Sorry, you must be at least 18 years old to register.");
+            }
+        else{
+            System.out.println("Invalid title entered. Please try again.");
+        }
+        }
+    }
+        // negetive approch for performing the tasks and  validating the user inputs
+        // if (
+        //     !title.equals("Mr")
+        //     && !title.equals("Mrs")
+        //     && !title.equals( "Miss")
+        //     && !title.equals("Shri")
+        // ){
+        //     System.out.println("Invalid Input! Please Enter Again.");
+        //     return;
+        // }
+
+        // if (age<18 && age>0){
+        //     System.out.println("Sorry, you are not eligible for registration as per Indian law.\nPlease contact Adminstrator for further assistance.");
+        //     System.out.println("Sorry, you are not eligible for registration as per Indian law.");
+        //     return;
+        // }
+        // if (!pass1.equals(pass2)){
+        //     System.out.println("Password does not match. Please try again.");
+        //     return;
+        // }
+        // System.out.println("Sign Up successfull");
+
+        }
+}
