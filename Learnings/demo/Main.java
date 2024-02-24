fff9f6676d85a09ff15d631114bcd25ae67cd3a3
Vidyansh07 <Vidyansht@gmail.com>
2024-02-24 13:32:19 +0530

    This is the java Code at day 10

---
+++ Learnings/demo/Main.java
@@ -0,0 +1,78 @@
+import java.util.Scanner;
+
+// public class Main {
+//     public static void main(String [] args){
+        // Sorthand condition example
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Please Enter the name : " );
+//         String name = sc.nextLine();   
+//         String result = (10 ==10) ? "Passed" : "failed";
+//         System.out.println(name + " You "+ result + " The exam." );    
+//         sc.close();
+//     }
+// }
+// conditions is java
+
+// if condition 
+
+// switch condition 
+
+// sorthand conditions 
+
+// syntax of sorthand condition 
+    // String result = (expression) ? true : false 
+
+// public class Main{
+//     public static void main(String [] args){
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Please Enter your name : ");
+//         String name = scanner.nextLine();
+        
+//         String result =(name.equals("vaibhav"))? "Success":  "Failed";
+//         System.out.println("Success");
+
+//         scanner.close();
+
+//     }
+// }
+
+// public class Main{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Plese Enter a number : ");
+//         int num = sc.nextInt();
+//         String result = (num%2 == 0)? "Modulus is equals to the zero": "Modulus is not equals to the zero";
+//         System.out.println(result);
+//     }
+// }
+
+
+
+/*
+ * Write a program to check the large number between two numbers.
+ */
+
+// public class Main{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Plese Enter the first number :");
+//         int num1 = sc.nextInt();
+//         System.out.println("Plese Enter the second number : ");
+//         int num2 = sc.nextInt();
+//         String result = (num1> num2)? String.valueOf(num2) + "is smaller number" : num1 + "is larger number";
+//         System.out.println(result);
+//     }
+// }
+
+
+
+/*
+ * Array in java
+ */
+
+public class Main{
+    public static void main(String[] args) {
+        String value[]= {"23","vidya","vibhav","a"};
+        
+    }
+}
